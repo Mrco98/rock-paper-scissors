c3ca71994f11d2d31483023ea47a55ca0a91b92e
Mark <marklouistiongco@gmail.com>
2025-05-09 12:20:13 +0800

    Add logic for every round in the game

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let decision = Math.floor(Math.random() * 3 + 1);
     switch (decision) {
@@ -14,3 +17,23 @@ function getHumanChoice() {
     let input = prompt("Choose to play either rock, paper, or scissor:");
     return input;
 }
+
+function playRound(humanChoice, computerChoice) {
+    choice = humanChoice.toLowerCase();
+    if (choice === "rock" && computerChoice === "scissor" || 
+        choice === "paper" && computerChoice === "rock" || 
+        choice === "scissor" && computerChoice === "paper") {
+        console.log(`You win! ${choice} beats ${computerChoice}`);
+        ++humanScore;
+    } else if (choice === computerChoice) {
+        console.log(`You both chose ${choice}! It's a tie`)
+    } else {
+        console.log(`You lose! ${choice} loses to ${computerChoice}`)
+        ++computerChoice;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
